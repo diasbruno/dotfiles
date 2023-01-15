4497b40a247351e5f873dd1e019884790d020479
Bruno Dias <dias.h.bruno@gmail.com>
2023-01-15 10:52:56 -0300

    added ocaml setup.

---
+++ emacs/ocaml-setup.el
@@ -0,0 +1,13 @@
+;;; ocaml-setup.el -- My emacs configuration.
+;;;
+;;; Commentary:
+;;;
+;;; Nothing to see here.
+;;;
+;;; Code:
+;;;
+
+(require 'use-package)
+
+(provide 'ocaml-setup)
+;;; ocaml-setup.el ends here
