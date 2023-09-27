0a76966972befb803c0b216b25ef46d93aad5353
Bruno Dias <dias.h.bruno@gmail.com>
2023-09-27 12:02:37 -0300

    update erlang related modes.

--- emacs/erlang-setup.el
+++ emacs/erlang-setup.el
@@ -9,24 +9,23 @@
 
 (require 'use-package)
 
-(defvar *erlang-asdf* "/dias/asdf/installs/erlang/22.2")
-
-(add-to-list 'exec-path (concat *erlang-asdf* "/bin"))
-
-;; (use-package erlang
-;;   :load-path "/dias/asdf/installs/erlang/22.2/lib/tools-3.3/emacs"
-;;   :custom ((erlang-root-dir *erlang-asdf*)
-;;            (erlang-man-root-dir (concat *erlang-asdf* "/man"))
-;;            (erlang-indent-level 2)
-;;            (inferior-erlang-machine-options '("-sname" "emacs"))
-;;            (inferior-erlang-prompt-timeout t))
-;;   :config (require 'erlang-start))
+(use-package erlang
+  :ensure t
+  :custom ((erlang-indent-level 2)
+           (inferior-erlang-machine-options '("-sname" "emacs"))
+           (inferior-erlang-prompt-timeout t)))
 
 (use-package alchemist
   :ensure t)
 
+(defun enable-paredit ()
+  "Enable paredit-mode."
+  (call-interactively #'paredit-mode))
+
 (use-package lfe-mode
-  :ensure t)
+  :ensure t
+  :custom ((indent-tabs-mode nil))
+  :hook ((lfe-mode-hook . #'enable-paredit)))
 
 (provide 'erlang-setup)
 ;;; erlang-setup.el ends here
