ba9f0e45e92e170b8cd6998a062012f709638b62
Bruno Dias <dias.h.bruno@gmail.com>
2023-09-27 12:02:52 -0300

    update haskell related modes.

--- emacs/haskell-setup.el
+++ emacs/haskell-setup.el
@@ -13,7 +13,12 @@
   :ensure t)
 
 (use-package lsp-haskell
-  :ensure t)
+  :ensure t
+  :config (progn
+            (setf lsp-haskell-server-path "haskell-language-server-wrapper")))
+
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-literate-mode-hook #'lsp)
 
 (provide 'haskell-setup)
 ;;; haskell-setup.el ends here
