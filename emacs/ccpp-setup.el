56d7e454e5872c22f139cdcff1ba2907a807f7a2
Bruno Dias <dias.h.bruno@gmail.com>
2026-09-12 10:41:17 -0300

    Update C/C++ mode tooling

--- emacs/ccpp-setup.el
+++ emacs/ccpp-setup.el
@@ -8,10 +8,19 @@
 ;;;
 
 
-(add-to-list 'load-path "~/Programming/c3-ts-mode/")
+;; (add-to-list 'load-path "~/programming/c3-ts-mode/")
 
-(require 'c3-ts-mode)
-(setf lsp-c3-c3-language-server-path "/Users/dias/Programming/c3-lsp/server/bin/c3lsp")
+;; (require 'c3-ts-mode)
+;; (setf lsp-c3-c3-language-server-path "/users/dias/programming/c3-lsp/server/bin/c3lsp")
+
+(straight-use-package
+ '(shader-mode :host github :repo "midnightSuyama/shader-mode"))
+
+(straight-use-package
+ '(clang-format :host github :repo "sonatard/clang-format"))
+
+(require 'clang-format)
+(setf clang-format-binary "/nix/store/j6rw6rmqj0gpck8ydrm8phz64kkfrzk9-clang-tools-21.1.8/bin/clang-format")
 
 (provide 'ccpp-setup)
 ;;; ccpp-setup.el ends here
